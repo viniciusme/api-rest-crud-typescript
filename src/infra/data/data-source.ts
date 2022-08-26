1782eb5a6dc8506749ebe1cb642edfb5638cf7a3
Vinicius Mendes <vini@vinimendes.com.br>
2022-08-25 22:09:07 -0300

    Feat: Refactoring coding

--- src/infra/data/data-source.ts
+++ src/infra/data/data-source.ts
@@ -1,14 +1,26 @@
+import { Console } from "console";
+import * as dotenv from "dotenv";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { users } from "../../core/entity/User";
 
+const DB_TYPE = process.env.TYPEORM_CONNECTION as
+  | "mysql"
+  | "mariadb"
+  | "mongodb";
+const DB_NAME = process.env.TYPEORM_DATABASE;
+const DB_HOST = process.env.TYPEORM_HOST;
+const DB_PORT = Number(process.env.TYPEORM_PORT);
+const DB_USERNAME = process.env.TYPEORM_USERNAME;
+const DB_PASSWORD = process.env.TYPEORM_PASSWORD;
+
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "P3nt3st@w3b",
-  database: "api-rest-crud",
+  type: DB_TYPE,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   synchronize: true,
   logging: false,
   entities: [users],
